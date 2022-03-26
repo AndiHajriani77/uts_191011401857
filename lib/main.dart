e58e83257008f9c53bd96f4e77e4591e53969677
Andi Hajriani <91936561+AndiHajriani77@users.noreply.github.com>
2022-03-26 10:57:45 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,61 +2,131 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(new MaterialApp(
-    title: "Grid dan Hero",
-    home: new Home(),
+    home: new Myapp(),
   ));
 }
 
-class Home extends StatelessWidget {
-  // This widget is the root of your application.
+class Myapp extends StatelessWidget {
   @override
-  _HomeState createdState() => new _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  List<Container> daftarSuperhero = new ListView();
-
-  var karakter = [
-    {"nama": "FastForious1", "gambar": "1.jpg"},
-    {"nama": "FastForious2", "gambar": "2.jpg"},
-    {"nama": "FastForious3", "gambar": "3.jpg"},
-    {"nama": "FastForious4", "gambar": "4.jpg"},
-    {"nama": "FastForious5", "gambar": "5.jpg"},
-    {"nama": "FastForious6", "gambar": "6.jpg"},
-    {"nama": "FastForious7", "gambar": "7.jpg"},
-    {"nama": "FastForious8", "gambar": "8.jpg"},
-    {"nama": "FastForious9", "gambar": "9.jpg"},
-    {"nama": "FastForious10", "gambar": "10.jpg"},
-  ];
-
-  _buatlist() async {
-    for (var i = 0; i < karakter.length; i++) {
-      final karakternya = karakter[i];
-      daftarSuperhero.add(
-        new Container(
-          child: new Text(karakternya["nama"]),
-        ),
-      );
-    }
-  }
-
-  @override
-  void initState() {
-    _buatlist();
-    super.initState();
-  }
-
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(
-          "Super Hero",
-          style: new TextStyle(color: Colors.white),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: new Text("Andi Hajriani"),
       ),
-      body: new GridView.count(
-        crossAxisCount: 2,
-        children: daftarSuperhero,
+      body: new Container(
+        child: GridView.count(
+          crossAxisCount: 3,
+          children: [
+            Container(
+              margin: EdgeInsets.all(20.0),
+              alignment: Alignment.center,
+              color: Colors.blue,
+              child: Text(
+                "1",
+                style: TextStyle(color: Colors.white, fontSize: 30.0),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(20.0),
+              alignment: Alignment.center,
+              color: Colors.blue,
+              child: Text(
+                "2",
+                style: TextStyle(color: Colors.white, fontSize: 30.0),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(20.0),
+              alignment: Alignment.center,
+              color: Colors.blue,
+              child: Text(
+                "3",
+                style: TextStyle(color: Colors.white, fontSize: 30.0),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(20.0),
+              alignment: Alignment.center,
+              color: Colors.blue,
+              child: Text(
+                "4",
+                style: TextStyle(color: Colors.white, fontSize: 30.0),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(20.0),
+              alignment: Alignment.center,
+              color: Colors.blue,
+              child: Text(
+                "5",
+                style: TextStyle(color: Colors.white, fontSize: 30.0),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(20.0),
+              alignment: Alignment.center,
+              color: Colors.blue,
+              child: Text(
+                "6",
+                style: TextStyle(color: Colors.white, fontSize: 30.0),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(20.0),
+              alignment: Alignment.center,
+              color: Colors.blue,
+              child: Text(
+                "7",
+                style: TextStyle(color: Colors.white, fontSize: 30.0),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(20.0),
+              alignment: Alignment.center,
+              color: Colors.blue,
+              child: Text(
+                "8",
+                style: TextStyle(color: Colors.white, fontSize: 30.0),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(20.0),
+              alignment: Alignment.center,
+              color: Colors.blue,
+              child: Text(
+                "9",
+                style: TextStyle(color: Colors.white, fontSize: 30.0),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(20.0),
+              alignment: Alignment.center,
+              color: Colors.blue,
+              child: Text(
+                "10",
+                style: TextStyle(color: Colors.white, fontSize: 30.0),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(20.0),
+              alignment: Alignment.center,
+              color: Colors.blue,
+              child: Text(
+                "11",
+                style: TextStyle(color: Colors.white, fontSize: 30.0),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(20.0),
+              alignment: Alignment.center,
+              color: Colors.blue,
+              child: Text(
+                "12",
+                style: TextStyle(color: Colors.white, fontSize: 30.0),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
